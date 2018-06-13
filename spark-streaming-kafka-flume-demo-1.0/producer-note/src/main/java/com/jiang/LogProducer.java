ba0ae2511f61dbf610499d92d5995d746aabb75e
jiangme <2587556966@qq.com>
2018-06-13 15:08:48 +0800

    新增HTML解析器->Jsoupp

--- spark-streaming-kafka-flume-demo-1.0/producer-note/src/main/java/com/jiang/LogProducer.java
+++ spark-streaming-kafka-flume-demo-1.0/producer-note/src/main/java/com/jiang/LogProducer.java
@@ -2,6 +2,9 @@ package com.jiang;
 
 import com.jiang.task.SpiderTask;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 /**
  * @author ajiang
  * @create by 18-6-11 下午8:24
@@ -9,7 +12,8 @@ import com.jiang.task.SpiderTask;
 public class LogProducer {
 
     public static void main(String[] args) {
-        SpiderTask task = new SpiderTask("http://www.paixin.com/", 5000L);
-        new Thread(task).start();
+        SpiderTask task = new SpiderTask("https://pixabay.com/zh/photos/", 50000L);
+        ExecutorService executorService = Executors.newFixedThreadPool(5);
+        executorService.submit(task);
     }
 }
--- spark-streaming-kafka-flume-demo-1.0/producer-note/src/main/java/com/jiang/task/SpiderTask.java
+++ spark-streaming-kafka-flume-demo-1.0/producer-note/src/main/java/com/jiang/task/SpiderTask.java
@@ -1,8 +1,11 @@
 package com.jiang.task;
 
+import com.jiang.bean.Image;
 import com.jiang.spider.SpiderDataFromInternet;
 import org.apache.log4j.Logger;
 
+import java.util.HashSet;
+import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -24,29 +27,49 @@ public class SpiderTask implements Runnable{
     }
 
     @Override
-    public void run() {
+    public void run(){
 
-        String regex = "http://[\\w+\\.?/?]+\\.[A-Za-z]+";
-        Pattern pattern = Pattern.compile(regex);
+           /*
 
-        while (true){
-            String str = SpiderDataFromInternet.strSpiderData(url);
-            String [] datas = str.split("\n");
-            log.info("start execute.....");
-            for(String data : datas){
-                Matcher matcher = pattern.matcher(data);
-                if(matcher.find()){
-                    String group = matcher.group();
-                    log.info("info : " + group);
+            \\b -> 空格 或 标点符号（， 、 。）
+            \\s -> space tab  enter 键
+
+         */
+        try {
+            String imgTab = "<img.*? src=\"?(.*?\\.(jpg|gif|bmp|bnp|png))\".*?>";
+            Pattern imgTabPattern = Pattern.compile(imgTab);
+            String img = "http[s]?://(w+|[a-zA-Z]*).\\w*.\\w*(/\\w*-?\\d*_*\\w*)*.(jpg|png|jpeg)";
+            Pattern imgPattern = Pattern.compile(img);
+            String alt = "\"\\W*\"";
+            Pattern altPattern = Pattern.compile(alt);
+
+            while (true){
+                String str = SpiderDataFromInternet.strSpiderData(url);
+                String [] datas = str.split("\n");
+                log.info("start execute.....");
+                for(String data : datas){
+                    Matcher matcher = imgTabPattern.matcher(data);
+                    while (matcher.find()){
+                        String imgTabContent = matcher.group();
+                        Matcher imgUrlMatcher = imgPattern.matcher(imgTabContent);
+                        Set<String> urls = new HashSet<>();
+                        while (imgUrlMatcher.find()){
+                            urls.add(imgUrlMatcher.group());
+                        }
+                        Matcher altMatcher = altPattern.matcher(imgTabContent);
+                        String altStr = "";
+                        if(altMatcher.find()){
+                            altStr = altMatcher.group();
+                        }
+                        Image image = new Image(urls, altStr);
+                        log.info("image infomation:");
+                        log.info(image);
+                    }
                 }
-            }
-
-            try {
                 Thread.sleep(time);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
             }
-
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
